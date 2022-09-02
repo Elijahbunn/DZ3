d57c7ceff4acef76374a83552465cc3f502b9683
Elijahbunn <110155080+Elijahbunn@users.noreply.github.com>
2022-09-02 22:13:06 +0300

    commit 1

---
+++ DZ2.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите проверяемое число: ");
+int current = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int degr = int.Parse(Console.ReadLine());
+
+double Deep = current;
+
+for (int numb = 2; numb <= degr; numb++)
+{
+Deep = Deep * current;
+}
+
+Console.WriteLine(Deep); 
